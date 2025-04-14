cc7f5e28c24f0c8cb4b27de1da08b4e6f0be986f
denxx145 <berlind445@gmail.com>
2025-04-14 18:15:51 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function scrollDown() {
+    window.scrollTo({top: window.innerHeight, behavior: 'smooth' });
+}
+
+
+let button = document.querySelector('.arrow-next')
+addEventListener('click', scrollDown())
+
+
+let team1 = document.querySelector('.team1')
+let team2 = document.querySelector('.team2')
+let team3 = document.querySelector('.team3')
+let height = Math.max(parseInt(getComputedStyle(team1).getPropertyValue('height')), parseInt(getComputedStyle(team2).getPropertyValue('height')), parseInt(getComputedStyle(team3).getPropertyValue('height')))
+team1.style.height = `${height}px`
+team2.style.height = `${height}px`
+team3.style.height = `${height}px`
+
